ad9fb6b56cc42973f22cd1131f2fafb4fe6f4433
vudieulinh162-stack <vudieulinh162@gmail.com>
2026-04-27 22:56:49 +0700

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com', 'images.unsplash.com'],
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
